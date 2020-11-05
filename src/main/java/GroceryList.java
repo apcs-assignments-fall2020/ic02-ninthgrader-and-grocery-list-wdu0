09a7efb675f2fe19df869ef292950d6c5ce47e7f
wdu0 <23wdu@athenian.org>
2020-11-05 09:20:58 +0000

    Added some code to the methods

--- src/main/java/GroceryList.java
+++ src/main/java/GroceryList.java
@@ -1,3 +1,42 @@
 public class GroceryList {
+
+    // Instance variables
+    private String[] arr;
     
+    // Constructors
+    public GroceryList() { 
+        arr = new String[10];
+    }
+
+    // Behaviors
+    public void add(String item) { 
+        for (int i=0; i<arr.length; i++) {
+            if (arr[i] == null) {
+                arr[i] = item;
+                break;
+            }
+        }
+    }
+
+    public void remove(String item) { 
+        for (int i=0; i<arr.length; i++) {
+            if (arr[i] == item) {
+                for (int p=i; p<(arr.length-1); p++) {
+                    arr[p] = arr[(p+1)];
+                }
+                arr[(arr.length-1)] = null;
+            }
+        }
+    }
+
+    public String toString() { 
+        String str = "";
+        for (int i=0; i<arr.length; i++) {
+            if (!(arr[i] == null)) {
+                str += arr[i];
+                str += " ";
+            }
+        }
+        return "Grocery List: " + str;
+    }
 }
--- src/main/java/NinthGrader.java
+++ src/main/java/NinthGrader.java
@@ -1,3 +1,24 @@
-public class NinthGrader {
+public class NinthGrader extends Student{
+    
+    // constructor
+    public NinthGrader(String a, String b) {
+        super(a, b, 2024);
+    }
+
+    public NinthGrader() {
+        super();
+    }
+
+    // doSomething()
+    public void doSomething() {
+        System.out.println(this.getFirstName() + " is doing physics' homework.");
+    }
+
+    // toString()
+    public String toString() {
+        String str = this.getFirstName() + " " + this.getLastName() + ", " + this.getClassYear();
+        return str;
+    }
+
     
 }
